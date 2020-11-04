e70b14b0d3376559d6ab3fe280cc159e7fba4878
svalenciasan <52926410+svalenciasan@users.noreply.github.com>
2020-11-03 18:25:46 -0600

    Deleted unused class and also edited the cMakeList accordingly.

---
+++ tests/test_particle_manager.cpp
@@ -0,0 +1,59 @@
+#include <catch2/catch.hpp>
+#include <core/particle.h>
+#include <core/particle_manager.h>
+
+using idealgas::particle::Particle;
+using idealgas::particlemanager::ParticleManager;
+
+TEST_CASE("Update", "[update]") {
+    SECTION("Update <0,0> velocity") {
+    vec2 position(0,0);
+    vec2 velocity(0,0);
+    Particle particle(10, position, velocity);
+    particle.Update();
+    REQUIRE(particle.GetPosition() == vec2(0,0));
+    }
+  SECTION("Update <0,1> velocity") {
+    vec2 position(0,0);
+    vec2 velocity(0,1);
+    Particle particle(10, position, velocity);
+    particle.Update();
+    REQUIRE(particle.GetPosition() == vec2(0,1));
+  }
+  SECTION("Update <1,0> velocity") {
+    vec2 position(0,0);
+    vec2 velocity(1,0);
+    Particle particle(10, position, velocity);
+    particle.Update();
+    REQUIRE(particle.GetPosition() == vec2(1,0));
+  }
+  SECTION("Update <1,1> velocity") {
+    vec2 position(0,0);
+    vec2 velocity(1,1);
+    Particle particle(10, position, velocity);
+    particle.Update();
+    REQUIRE(particle.GetPosition() == vec2(1,1));
+  }
+  //Negative
+  SECTION("Update <0,-1> velocity") {
+    vec2 position(0,0);
+    vec2 velocity(0,-1);
+    Particle particle(10, position, velocity);
+    particle.Update();
+    REQUIRE(particle.GetPosition() == vec2(0,-1));
+  }
+  SECTION("Update <-1,0> velocity") {
+    vec2 position(0,0);
+    vec2 velocity(-1,0);
+    Particle particle(10, position, velocity);
+    particle.Update();
+    REQUIRE(particle.GetPosition() == vec2(-1,0));
+  }
+  SECTION("Update <-1,-1> velocity") {
+    vec2 position(0,0);
+    vec2 velocity(-1,-1);
+    Particle particle(10, position, velocity);
+    particle.Update();
+    REQUIRE(particle.GetPosition() == vec2(-1,-1));
+  }
+}
